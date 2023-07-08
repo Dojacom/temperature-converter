349f478fabe6220d8d24204c35e93789d47a3d40
Ismail Oladoja <olansileismail1@gmail.com>
2023-07-08 06:41:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const celciusEl = document.getElementById("celcius")
+const farenheitEl = document.getElementById("farenheit")
+const kelvinEl = document.getElementById("kelvin")
+
+function calculateTemp(event){
+    const currentValue = +event.target.value
+
+    switch (event.target.name) {
+        case "celcius":
+            kelvinEl.value = (currentValue + 273.32)
+            farenheitEl.value = (currentValue * 1.8 + 32)
+            break;
+    case "farenheit":
+        celciusEl.value = ((currentValue -32) / 1.8)
+        kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32)
+        break;
+        case "kelvin":
+            celciusEl.vlaue = (currentValue - 273.32)
+            farenheitEl.value = ((currentValue - 273.32) * 1.8 + 32)
+            break;
+        default:
+            break;
+    }
+
+}
